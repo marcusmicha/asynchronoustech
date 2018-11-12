0fd31ea68f1d246626a3a1501a76ea9b13b1364c
marcusmicha <marcusmicha@users.noreply.github.com>
2018-11-12 14:18:59 +0100

    New version

--- handles.js
+++ handles.js
@@ -1,17 +1,17 @@
 // ./handles.js
-// const content = '<!DOCTYPE html>' +
-// '<html>' +
-// '    <head>' +
-// '        <meta charset="utf-8" />' +
-// '        <title>ECE AST</title>' +
-// '    </head>' + 
-// '    <body>' +
-// '         <p>Hello World !</p>' +
-// '    </body>' +
-// '</html>'
 
 const url = require('url')
 const qs = require('querystring')
+const defaultContent = '<p style="text-align: center;">Hi everyone</p>'
++'<p style="text-align: center;">This application lets you display the name of anyone.</p>'
++'<p style="text-align: center;">You will have more details if you try with "<em>Michael</em>"</p>'
++'<p style="text-align: center;">Enjoy !</p>'
+
+const nameContent = '<p style="text-align: center;">Bonjour, je m\'appelle Michael.</p>'
++'<p style="text-align: center;">Je suis &eacute;tudiant en 5e ann&eacute;e &agrave; l\'ECE Paris.</p>'
++'<p style="text-align: center;">J\'aime l\'informatique, l\'aventure et pratique le short track</p>'
++'<p style="text-align: center;">&Agrave; bient&ocirc;t !</p>'
+
 
 module.exports = {
     serverHandle: function (req, res) {
@@ -19,15 +19,23 @@ module.exports = {
         const path = route.pathname 
         const params = qs.parse(route.query)
 
-
-        res.writeHead(200, {'Content-Type': 'text/plain'});
         // res.end(content);
-        if (path === '/hello' && 'name' in params) {
-            res.write('Hello ' + params['name'])
-            res.end()
-          } else {
-            res.write('Hello anonymous')
-            res.end()
+        if (path == '/') {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(defaultContent);
+            res.end();
+        }
+        else if (path === '/hello' && 'name' in params) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write('Hello ' + params['name']);
+            if (params['name'] === "michael" || params['name'] === "Michael")
+                res.write(nameContent);
+            res.end();
+        }
+        else {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.write('404, Not Found');
+            res.end();
           }
     } 
   }
